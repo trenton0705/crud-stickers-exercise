0fadd7f4c0c2374ade8b9c0dfde5a04721def066
Trenton Wuerker <trenton.wuerker@gmail.com>
2017-07-20 16:01:08 -0600

    finished

--- api/stickers.js
+++ api/stickers.js
@@ -1,4 +1,50 @@
 const express = require('express');
 const router = express.Router();
+var knex = require('../db/knex')
+
+router.get('/', (req, res) => {
+  knex('sticker')
+    .then(stickers => {
+      res.json(stickers)
+    })
+})
+
+router.get('/:id', (req, res) => {
+  let id = req.params.id
+
+  knex('sticker').where('id', id).first()
+    .then(stickers => {
+      res.json(stickers)
+    })
+})
+
+router.post('/', (req, res) => {
+  let post = req.body
+  knex('sticker').insert(post)
+  .returning('*')
+    .then(stickers => {
+      res.json(stickers[0])
+    })
+})
+
+router.put('/:id', (req, res) => {
+  let id = req.params.id
+
+  knex('sticker').where('id', id)
+  .update(req.body)
+  .returning('*')
+    .then(stickers => {
+      res.json(stickers[0])
+    })
+})
+
+router.delete('/:id', (req, res) => {
+  let id = req.params.id
+
+  knex('sticker').where('id', id).del()
+  .then(deleted => {
+    res.json({"deleted": true})
+  })
+})
 
 module.exports = router;
--- knexfile.js
+++ knexfile.js
@@ -3,7 +3,7 @@
 module.exports = {
   development: {
     client: 'pg',
-    connection: 'postgres://localhost/stickers'
+    connection: 'postgres://localhost/sticker'
   },
   test: {
     client: 'pg',
